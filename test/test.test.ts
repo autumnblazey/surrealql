b29a649dff03c7b1ddb531b9affd26023a450278
Autumn Meadow <blazeykirin@gmail.com>
2022-09-12 18:41:47 -0700

    initialise tests and finess with it until it works?

---
+++ test/test.test.ts
@@ -0,0 +1,9 @@
+import { create_surrealql } from "..";
+import { describe, test } from "mocha";
+import { assert } from "./testutil";
+
+describe("test test", () => {
+	test("works?", () => {
+		assert(true, "it true");
+	});
+});
